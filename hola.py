5b2c96774776ea56e0512299fc3a2a8b66abebff
SystemManager <deumanaro@gmail.com>
2014-09-03 16:56:02 -0500

    nueva linea de elkin

--- hola.py
+++ hola.py
@@ -1,2 +1,15 @@
 print hola mundo
 print adiós mundo
+
+"""
+con git add se adiciona
+con git commit -m se adiciona un mensaje al archivo, una etiqueta de tiempo
+
+con git push se sincroniza con los servidores en California
+
+en la página, con settings se agregan colaboradores al repositorio
+
+con blame se puede ver si hubo modificaciones y quién y cuándo se realizaron
+
+por último con git pull se refresca el contenido en el equipo propio
+"""
